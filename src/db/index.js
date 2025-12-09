c9bf8022891c6fb835e4f162143a38c6ba5cfdaf
Farheen Irfan <farheenirfan4040@gmail.com>
2025-12-09 12:34:21 +0500

    connect to mongodb

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose, { modelNames } from "mongoose";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`);
+        console.log(`\n MongoDb connected. DB Host: ${connectionInstance.connection.host}`);
+        
+        
+    } catch (error) {
+        console.log("MongoDb coneection error ", error);
+        process.exit(1)
+        
+
+        
+    }
+}
+
+export default connectDB
