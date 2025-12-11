1229fcfba15b213d616b9775a54313268d6aa58b
Farheen Irfan <farheenirfan4040@gmail.com>
2025-12-11 14:55:57 +0500

    Add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -176,7 +176,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     );
 
     const user = await User.findById(decodedToken?._id);
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
       throw new ApiError(401, "Unauthorized request.");
     }
 
